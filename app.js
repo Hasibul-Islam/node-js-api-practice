85d791d80fecef8e1aeeba54325f20ca230a56c5
Md. Hasibul Islam <polok.hasibul@gmail.com>
2021-07-02 23:40:43 +0600

    done all crud method except update

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+//Required packages
+
 const express = require('express')
 
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+const mongoose = require('mongoose',{ useUnifiedTopology: true })
+
+const postRoute = require('./routes/posts')
+
+app.use('/posts',postRoute)
+
+
+require('dotenv/config')
+
+//ROUTES
 
 app.get('/',(req,res)=>
 		
@@ -14,5 +28,18 @@ app.get('/posts',(req,res)=>
 			res.send('We are at posts!')
 		}	
 	)
+
+//Connect to DB
+
+mongoose.connect(
+	process.env.DB_CONNECTION,
+	{ useNewUrlParser: true },
+
+	() => 
+	console.log('Connected to db')
+	)
+
+
+//Connect to server
 
 app.listen(3000)
